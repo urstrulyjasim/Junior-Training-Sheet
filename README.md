4596782ad3c4c6bb47de3b5dca3274b53f105138
urstrulyjasim <mohammadjasim229@gmail.com>
2023-12-24 02:57:46 +0600

    Adding new Information

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Junior-Training-Sheet
+Repository for Codeforces JTS
+<br>
+Author -Jasim
---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+//بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include<bits/stdc++.h>
+using namespace std;
+       
+int main()
+{
+    cout<<"Hello World!"<<endl;
+    
+    return 0;
+}
